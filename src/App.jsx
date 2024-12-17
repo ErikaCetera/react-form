e7ddd16c3cddd47149c2edf481f8af5316922e08
ErikaCetera <ceteraerika@gmail.com>
2024-12-17 15:53:59 +0100

    feat: form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react'
 
 function App() {
+  // Definisce uno stato chiamato 'title' 
+ const [title, setTitle ] = useState("");
+
+ // Funzione per gestire il cambiamento del valore dell'input
+ const handleChange = (event) => { 
+  const newValue = event.target.value;
+  console.log(newValue);
   
+  setTitle(newValue);
+ };
+
+ // Funzione per gestire l'invio del form
+ const handleSubmit = (event) => {
+  event.preventDefault();
+  
+};
 
   return (
     <>
@@ -10,11 +25,19 @@ function App() {
       </header>
       <main className='container mt-5'>
         <div className='row w-50'>
-          <form >
-
-            <label className='form-label' htmlFor="title">Titolo</label>
-            <input className='form-control' type="text" id='title' />
-
+          <form onSubmit={handleSubmit} >
+            <div className='mb-3'>
+              <label className='form-label' htmlFor="title">Titolo</label>
+              <input
+               className='form-control' 
+               type="text" id='title'
+               // Imposta il valore dell'input come 'title'
+               value = {title}
+               // Chiama handleChange quando il valore dell'input cambia
+               onChange={handleChange}
+               />
+            </div>
+            <button type="submit">Invia</button>
           </form>
         </div>
       </main>
